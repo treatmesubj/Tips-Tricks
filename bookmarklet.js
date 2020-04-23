aec42140f47806196a7267401780dbe2e3a497c6
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-04-22 22:05:04 -0500

    oneliner

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-//javascript:(function(){ var e = document.getElementsByTagName("*"); for(var i = 0; i < e.length; i++) { e[i].style.fontFamily = "Comic Sans MS"; } })()
+javascript:(function(){ var e = document.getElementsByTagName("*"); for(var i = 0; i < e.length; i++) { e[i].style.fontFamily = "Comic Sans MS"; } })()
