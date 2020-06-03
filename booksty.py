68ac54916c541fbdc92d2ee248cd81b62eb9801d
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-06-02 19:47:36 -0500

    darkmode textbook

---
+++ booksty.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import os
+from shutil import copyfile
+
+
+files = os.listdir(os.getcwd())
+print(files)
+
+css_file = "phil_log.css"
+
+"""
+body {
+	background-color: black;
+}
+
+path {
+	fill: black;
+}
+
+div.page svg text {
+	fill: white;
+}
+"""
+
+os.makedirs(f"{os.getcwd()}\\dark")
+copyfile("phil_log.css", f"{os.getcwd()}\\dark\\{css_file}")
+
+for file in files:
+	if ".html" == file[-5:]:
+		print(f"working on {file}...")
+		html = open(file, 'r', encoding='utf-8')
+		soup = BeautifulSoup(html, features="lxml")
+		new_link = soup.new_tag("link", rel="stylesheet", type="text/css", href=css_file)
+		soup.head.append(new_link)
+
+		with open(f"{os.getcwd()}\\dark\\{file}", "w", encoding="utf-8") as f:
+			f.write(str(soup))
