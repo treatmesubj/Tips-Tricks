45e64265ca648c9163e9bec42f5c3cd79b09d1eb
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-08-26 15:46:24 -0500

    get cwd

--- url_content_dl.py
+++ url_content_dl.py
@@ -1,12 +1,10 @@
-import win32api
 import requests
 import os
 
 url = input("url: ")
 file_name = input("file name & extension: ")
 
-dir_loc = f"C:\\Users\\{win32api.GetUserName()}\\Desktop"
-file_path = f"{dir_loc}\\{file_name}"
+file_path = f"{os.getcwd()}\\{file_name}"
 if os.path.exists(file_path):
 	os.remove(file_path)
 with open(file_path, "wb") as f:
