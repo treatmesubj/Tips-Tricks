738a410ffdab03c2413b5c9ce744ea18d1791c66
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-12-16 15:38:15 -0600

    py boilerplate

---
+++ regex4json.py
@@ -0,0 +1,32 @@
+import regex  # conditional look-arounds
+
+reggy = r"(?<=var(.|\n)*?obj_name(.|\n)*?=(.|\n)*?)(?<!var(.|\n)*?obj_name(.|\n)*?=(.|\n)*?;(.|\n)*?){((.|\n)*?)};"
+
+text = """
+var breakfast = {
+	{
+		meal: "pancake",
+		drink: "oj"
+	}
+};
+
+var obj_name = { blah: "blah",
+	hey: "hey" 
+};
+
+var breakfast = {
+	{
+		meal: "pancake",
+		drink: "oj"
+	}
+};
+
+
+{
+}
+"""
+
+jaysean = regex.match(reggy, text)
+
+print(jaysean.group(0))
+
