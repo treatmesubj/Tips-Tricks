8550a60d8ada4bc2b57e237a4e12b14f54eb2641
treatmesubj <jrock4503@hotmail.com>
2021-04-20 10:46:14 -0500

    ps1 profile

---
+++ configs/custom_PS_cmds.ps1
@@ -0,0 +1,3 @@
+# $profile => C:\Users\JohnHupperts\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+Set-Alias -Name sublime -Value "C:\Program Files\Sublime Text 3\sublime_text.exe"
+function thesr {python "C:\Users\JohnHupperts\Documents\Programming_Projects\thesr.py" $args}
