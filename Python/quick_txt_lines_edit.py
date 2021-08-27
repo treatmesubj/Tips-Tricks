ff74fcb975c6d327d18848c9d681960d131bac19
treatmesubj <jrock4503@hotmail.com>
2021-08-27 08:21:01 -0500

    sys arg infile path

--- Python/quick_txt_lines_edit.py
+++ Python/quick_txt_lines_edit.py
@@ -1,14 +1,28 @@
-f = open('dummy.txt', 'r')
+"""
+reads in a text file's lines as a list.
+manipulates each line in the list.
+writes resulting lines to an outfile in directory of infile
+"""
+import os
+import sys
+
+
+in_file = sys.argv[1]
+
+# read infile as list of lines
+f = open(in_file, 'r')
 ls = f.readlines()
 f.close()
-new_ls = []
+out_ls = []
 
+# iterate lines, manipulate them, write to outlist
 for row in ls:
-	new_ls.append(row.split('WSDIW.')[1].strip())
+	# new_ls.append(row.split('WSDIW.')[1].strip())
+	out_ls.append(row.strip().upper())
+print(out_ls)
 
-print(new_ls)
-
-f = open('outfile.txt', 'w')
-for line in new_ls:
+# write out list to outfile
+f = open(f"{os.path.dirname(in_file)}\\out_file.txt", 'w')
+for line in out_ls:
 	f.writelines(f"{line}\n")
 f.close()
