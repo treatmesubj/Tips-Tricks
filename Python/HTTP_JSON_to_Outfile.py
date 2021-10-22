21672b8bb2b029257bca5e3029e23abc7bbd4d0d
treatmesubj <jrock4503@hotmail.com>
2021-10-22 16:17:25 -0500

    json to outfile

---
+++ Python/HTTP_JSON_to_Outfile.py
@@ -0,0 +1,13 @@
+import requests
+import json
+"""
+fetches a JSON response via HTTP request and writes its pretty response to an outfile in same directory as working directory
+"""
+
+
+response = requests.get("http://www.site.com?thing.search/byjson?*")
+sanjay = json.loads(response.text)
+
+
+with open('./response.json', 'w') as f:
+	print(json.dumps(sanjay, indent=4, sort_keys=True), file=f)
