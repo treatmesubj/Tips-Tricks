9c5e146aa7d7c0d8a91826d08c9b5ee222ffad4c
treatmesubj <jrock4503@hotmail.com>
2022-01-10 11:57:35 -0600

    cam req logging

--- Raspberry_Pi_Stuff/camera/camera.py
+++ Raspberry_Pi_Stuff/camera/camera.py
@@ -13,15 +13,22 @@ def now():
     return dt.now().strftime('%Y-%m-%d.%H:%M:%S')
 
 
-def log_exception(log_path, note=''):
-    with open(log_path, 'a') as log_file:
+def log_exception(exception_log_path, note=''):
+    with open(exception_log_path, 'a') as log_file:
         log_file.writelines(now() + ' >> ' + note + "\n" +
                                traceback.format_exc() + "\n")
 
 
+def log_Drive_request(request_log_path, Drive_upload_response, note=''):
+    with open(request_log_path, 'a') as log_file:
+        log_file.writelines(now() + ' >> ' + note + "\n" +
+                                Drive_upload_response.__repr__() + "\n")
+
+
 if __name__ == "__main__":
 
-    log_path = "/home/pi/Desktop/tracebacks.log"
+    exception_log_path = "/home/pi/Desktop/sec_cam_tracebacks.log"
+    request_log_path = "/home/pi/Desktop/sec_cam_requests.log"
 
     scopes = ['https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/Desktop/drive_service_creds.json', scopes)
@@ -43,13 +50,14 @@ if __name__ == "__main__":
                 'parents': ['1ih_b8pRyWZfBKZ_PEgWf3WL-hkXwZBtt']
             }
             media = MediaFileUpload('/home/pi/Desktop/image.jpg', mimetype='image/jpeg')
-            upload_response = drive.files().create(body=file_metadata, media_body=media, fields='id,name,mimeType,parents,properties,owners,driveId').execute()
+            Drive_upload_response = drive.files().create(body=file_metadata, media_body=media, fields='id,name,mimeType,parents,properties,owners,driveId').execute()
+            log_Drive_request(request_log_path, Drive_upload_response)
             if len(file_id_list) > 10:
                 delete_response = drive.files().delete(fileId=file_id_list[0]).execute()
                 file_id_list.pop(0)
-            latest_file_id = upload_response.get('id')
+            latest_file_id = Drive_upload_response.get('id')
             file_id_list.append(latest_file_id)
             time.sleep(5)
         except Exception:
             traceback.print_exc()
-            log_exception(log_path=log_path)
+            log_exception(exception_log_path=exception_log_path)
