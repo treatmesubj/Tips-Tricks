553c58b1e2bdd04a4fe96d555aac54761c975c20
treatmesubj <jrock4503@hotmail.com>
2022-01-21 11:33:40 -0600

    formatting

--- configs/PowerShell/Microsoft.PowerShell_profile.ps1
+++ configs/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,11 @@
 # $profile => C:\Users\JohnHupperts\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+
 Set-Alias -Name sublime -Value "C:\Program Files\Sublime Text 3\sublime_text.exe"
-function thesr {python "C:\Users\JohnHupperts\Documents\Programming_Projects\thesr.py" $args}
+
+function thesr {
+	python "C:\Users\JohnHupperts\Documents\Programming_Projects\thesr.py" $args
+}
+
 function prompt {
 	'PS [' + ($pwd -split '\\')[0] + '] ' + '[' + ($env:UserName) + '] ' + '\'+$(($pwd -split '\\')[-1] -join '\') + '> '
 }
