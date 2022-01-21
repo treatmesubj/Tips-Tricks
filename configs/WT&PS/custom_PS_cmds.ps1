d1dacb808adbe603b41bac53228a4fa9ecc65a18
treatmesubj <jrock4503@hotmail.com>
2022-01-21 11:32:11 -0600

    file shuffle

--- configs/WT&PS/custom_PS_cmds.ps1
+++
@@ -1,3 +0,0 @@
-# $profile => C:\Users\JohnHupperts\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-Set-Alias -Name sublime -Value "C:\Program Files\Sublime Text 3\sublime_text.exe"
-function thesr {python "C:\Users\JohnHupperts\Documents\Programming_Projects\thesr.py" $args}
