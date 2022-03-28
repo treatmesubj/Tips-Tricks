2192babfe5c02f47ce55a8f21d2cddb1726832a5
treatmesubj <jrock4503@hotmail.com>
2022-03-28 11:50:18 -0500

    sql & mit links

--- DB2_Querying.sql
+++ DB2_Querying.sql
@@ -11,45 +11,84 @@ QMF Ad Hoc Queries
 	# ini file: C:\ProgramData\IBM\DB2\IBMDBCL1\cfg\db2cli.ini
 */
 
-/* Select all Schemas from Host DB2 */
-SELECT * FROM SYSCAT.SCHEMATA;
+/* Looking for Schemas */
+SELECT *
+FROM SYSCAT.SCHEMATA
+WHERE SCHEMANAME = 'BLAH'
+WITH UR;
 
-/* Select all Tables in Schema */
-SELECT * FROM SYSIBM.SYSTABLES
-WHERE CREATOR = '<schema>';
-
-/* Select all Views in Schema */
-SELECT * FROM SYSIBM.SYSVIEWS
-WHERE CREATOR = '<schema>';
-
-/* Select all Columns in View or Table */
-SELECT * FROM SCHEMA.VIEW_NAME WHERE 1 = 0;
---or
-SELECT * FROM SYSIBM.SYSCOLUMNS
-WHERE TBNAME = '<table>'
-	AND TBCREATOR = '<schema>';
-
-/* Select all Records Where Column Like <pattern> */
-SELECT * FROM SCHEMA.TABLE
+/* Looking for Tables */
+SELECT NAME, CREATOR, TYPE, CTIME, BASE_NAME, BASE_SCHEMA, LAST_REGEN_TIME, ALTER_TIME, LASTUSED
+FROM SYSIBM.SYSTABLES
 WHERE (
-    COLUMN LIKE 'BLAH%'
-    OR COLUMN LIKE 'BLEH%'
-    OR COLUMN LIKE 'BLEK%'
-    OR COLUMN LIKE 'DUH%'
+	NAME IN 'BLAH'
+	OR NAME IN 'BLEH'
+	OR NAME IN 'BLEK'
 )
 WITH UR;
 
-/* Count number of Records in Query*/
-SELECT COUNT(*) AS ROW_COUNT
-FROM SCHEMA.TABLE;
-
-/* limit results to 10 rows */
-SELECT * FROM SCHMEMA.TABLE
-FETCH FIRST 10 ROWS ONLY
+SELECT NAME, CREATOR, TYPE, REMARKS
+FROM SYSIBM.SYSTABLES
+WHERE NAME IN ('BLAH', 'BLEH')
+    AND TYPE IN ('T', 'V')
 WITH UR;
 
-/* Sort Results */
+/* Looking for Views that SELECT from something else */
+SELECT NAME, CREATOR, TYPE, STATEMENT
+FROM SYSIBM.SYSVIEWS
+WHERE NAME IN ('BLAH', 'BLEH', 'BLEK')
+WITH UR;
+
+
+/* Looking for Columns */
+SELECT NAME, TBNAME, TBCREATOR, REMARKS, COLTYPE, LENGTH, NULLS
+FROM SYSIBM.SYSCOLUMNS
+WHERE (
+	NAME IN 'BLAH'
+	OR NAME IN 'BLEH'
+	OR NAME IN 'BLEK'
+)
+WITH UR;
+
+/* Look for who ingested */
 SELECT *
-FROM SCHEMA.TABLE
-ORDER BY COLUMN [ASC | DESC] --default ascending
+FROM COEDL_INGESTION.TIMES_V
+WHERE BIGSQLSCHEMA = 'SCHEMA'
+ORDER BY UPDATED [ASC|DESC]
 WITH UR;
+
+/* get row count of table */
+SELECT COUNT(*) FROM SCHEMA.TABLE
+/* Excel Formula for writing SQL to get row counts for list of tables in Excel 
+="SELECT '"&B3&"' TABLE_NAME, COUNT(*) RECORDS FROM WSDIW."&B3&" UNION"
+*/
+
+/* get row counts of all tables */
+-- Note STATS_TIME might be old, so CARD != COUNT(*)
+SELECT TABSCHEMA, TABNAME, TRIM(TABSCHEMA) CONCAT '.' CONCAT TRIM(TABNAME) AS FULL_TABLE_NAME,
+    CARD AS ROWS,
+    STATS_TIME
+FROM SYSCAT.TABLES
+ORDER BY CARD DESC 
+
+/* to get table organization (row vs column) from DB2 */
+SELECT TABSCHEMA, TABNAME, TYPE, TABLEORG 
+FROM SYSCAT.TABLES t 
+WHERE TABSCHEMA IN ('WSDIW_A', 'WSDIW_B', 'ECOSYSTEMS_IZ')
+
+/* Get counts of column values for column */
+SELECT COLUMN_NAME, COUNT(*)
+FROM SCHEMA_NAME.TABLE_NAME
+GROUP BY COLUMN_NAME;
+
+/* Cases */
+SELECT 
+	COLUMN_A, 
+	COLUMN_B,
+	CASE
+	   WHEN ...
+	   THEN ...
+	   ELSE ...
+	END AS COLUMN_C,
+	COLUMN_D
+FROM SCHEMA.TABLE
