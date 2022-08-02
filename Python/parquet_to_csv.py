4e16ebfdf9303242acce610db8291d7108feecd2
treatmesubj <jrock4503@hotmail.com>
2022-08-02 15:04:00 -0500

    parquet to csv

---
+++ Python/parquet_to_csv.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+
+
+if __name__ == "__main__":
+	df = pd.read_parquet(sys.argv[1], engine='fastparquet')
+	try:
+		df.to_csv(sys.argv[2])
+		print(sys.argv[2])
+	except IndexError:
+		df.to_csv(f"{sys.argv[1].split('.parquet')[0]}.csv")
+		print(f"{sys.argv[1].split('.parquet')[0]}.csv")
