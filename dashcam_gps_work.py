6163a93517aa258f40c0fde1bae9325e7d8815b2
treatmesubj <jrock4503@hotmail.com>
2022-08-27 23:59:54 -0500

    dashcam work

---
+++ dashcam_gps_work.py
@@ -0,0 +1,20 @@
+import os
+import re
+import subprocess
+
+
+reggy = re.compile(r"[A-Z]{2}RMC,(\d{2})(\d{2})(\d+(\.\d*)?),A?,(\d*?)(\d{1,2}\.\d+),([NS]),(\d*?)(\d{1,2}\.\d+),([EW]),(\d*\.?\d*),(\d*\.?\d*),(\d{2})(\d{2})(\d+)")
+
+for file in os.listdir():
+    print(file)
+    p = subprocess.run(["strings", file], capture_output=True, text=True)
+    p2 = subprocess.run(["grep", "-e" "GPRMC", "-e", "GPVTG", "-e", "GPGSA", "-e", "GPGSV", "-e", "GPGLL"], input=p.stdout, capture_output=True, text=True)
+    mp4_gps_metadata = p2.stdout
+
+    # TODO: appending all the metadata from the directory into 1 text file
+
+    for line in mp4_gps_metadata.splitlines():
+        print(line)
+        results = reggy.findall(line)
+        mph = float(results[0][10]) * 1.15078
+        date = f"{results[0][13]}/{results[0][12]}/{results[0][14]}"
