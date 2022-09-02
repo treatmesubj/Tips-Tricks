f30c0eca1fafc663abe7cd2fab50616cff5cf91f
treatmesubj <jrock4503@hotmail.com>
2022-09-02 14:49:39 -0500

    cmd line args

--- dashcam_gps_work.py
+++ dashcam_gps_work.py
@@ -1,18 +1,28 @@
 import os
+import sys
 import re
 import subprocess
 
 
-reggy = re.compile(r"[A-Z]{2}RMC,(\d{2})(\d{2})(\d+(\.\d*)?),A?,(\d*?)(\d{1,2}\.\d+),([NS]),(\d*?)(\d{1,2}\.\d+),([EW]),(\d*\.?\d*),(\d*\.?\d*),(\d{2})(\d{2})(\d+)")
+"""
+iterates files in a given directory of Thinkware dashcam MP4 files,
+parses their raw data for GPS metadata using GNU strings utility,
+and appends their GPS metadata to a text file
+"""
 
-for file in os.listdir():
-    print(file)
-    p = subprocess.run(["strings", file], capture_output=True, text=True)
+reggy = re.compile(r"[A-Z]{2}RMC,(\d{2})(\d{2})(\d+(\.\d*)?),A?,(\d*?)(\d{1,2}\.\d+),([NS]),(\d*?)(\d{1,2}\.\d+),([EW]),(\d*\.?\d*),(\d*\.?\d*),(\d{2})(\d{2})(\d+)")
+# results = reggy.findall(line)
+# mph = float(results[0][10]) * 1.15078
+# date = f"{results[0][13]}/{results[0][12]}/{results[0][14]}"
+
+for file_name in os.listdir(sys.argv[1]):
+    if not file_name.endswith(".MP4"):
+        continue
+    print(file_name)
+    p = subprocess.run(["strings", f"{sys.argv[1]}/{file_name}"], capture_output=True, text=True)
     p2 = subprocess.run(["grep", "-e" "GPRMC", "-e", "GPVTG", "-e", "GPGSA", "-e", "GPGSV", "-e", "GPGLL"], input=p.stdout, capture_output=True, text=True)
     mp4_gps_metadata = p2.stdout
+    print(mp4_gps_metadata)
 
-    with open('mp4_gps_metadata.gps', 'a') as filey:
-        filey.writelines(mp4_gps_metadata)
-        # results = reggy.findall(line)
-        # mph = float(results[0][10]) * 1.15078
-        # date = f"{results[0][13]}/{results[0][12]}/{results[0][14]}"
+    with open('mp4_gps_metadata.gps', 'a') as gps_outfile:
+        gps_outfile.writelines(mp4_gps_metadata)
