3681bbf0edceb4463b4e88732bb8bb14792bbc20
treatmesubj <jrock4503@hotmail.com>
2022-11-11 10:40:22 -0600

    typo

--- Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
+++ Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
@@ -3,4 +3,4 @@ sudo fbi -a -vt 2 /mnt/usb_share/holding_big_gar.jpeg  # view image on tty2, if 
 
 sudo openvt -s -l -- fim /mnt/usb_share/john_w_kirk.jpeg # create and switch to tty2 just to view an image
 sudo chvt 1  # switch back to tty1
-sudo kill - 9 $(pgrep fim)  # kill the fbi process
+sudo kill -9 $(pgrep fim)  # kill the fbi process
