af711f221b656594ad88ad40ac0a3117a4560708
treatmesubj <jrock4503@hotmail.com>
2022-11-11 09:13:08 -0600

    openvt

--- Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
+++ Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
@@ -1,1 +1,5 @@
-sudo fbi -a -vt 1 /mnt/usb_share/holding_big_gar.jpeg
+sudo fbi -a -vt 1 /mnt/usb_share/holding_big_gar.jpeg  # view image on tty1
+sudo fbi -vt 1 -q  # quit fbi
+
+sudo openvt -c 2 -f -s -- fbi -a /mnt/usb_share/holding_big_gar.jpeg  # create and switch to tty2 just to view an image
+# sudo kill - 9 <fbi's pid>  # kill the fbi process
