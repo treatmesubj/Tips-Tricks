38e4a67a64407e2fa3f9acbff0b5f4a2ca3fe776
treatmesubj <jrock4503@hotmail.com>
2022-11-11 09:26:11 -0600

    tty2

--- Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
+++ Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
@@ -1,5 +1,5 @@
-sudo fbi -a -vt 1 /mnt/usb_share/holding_big_gar.jpeg  # view image on tty1
-sudo fbi -vt 1 -q  # quit fbi
+sudo fbi -a -vt 2 /mnt/usb_share/holding_big_gar.jpeg  # view image on tty2, if it exists
+sudo fbi -vt 2 -q  # quit fbi
 
 sudo openvt -c 2 -f -s -- fbi -a /mnt/usb_share/holding_big_gar.jpeg  # create and switch to tty2 just to view an image
 sudo chvt 1  # switch back to tty1
