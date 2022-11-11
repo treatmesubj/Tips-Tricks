2add1802f690c64207230bff715b3dcd538d3c28
treatmesubj <jrock4503@hotmail.com>
2022-11-11 10:39:09 -0600

    pgrep

--- Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
+++ Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
@@ -1,6 +1,6 @@
 # fim is basically fib with more options
 sudo fbi -a -vt 2 /mnt/usb_share/holding_big_gar.jpeg  # view image on tty2, if it exists
 
-sudo openvt -s -l -- fim --no-commandline /mnt/usb_share/john_w_kirk.jpeg # create and switch to tty2 just to view an image
+sudo openvt -s -l -- fim /mnt/usb_share/john_w_kirk.jpeg # create and switch to tty2 just to view an image
 sudo chvt 1  # switch back to tty1
-# sudo kill - 9 <fbi's pid>  # kill the fbi process
+sudo kill - 9 $(pgrep fim)  # kill the fbi process
