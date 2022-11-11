f10c7784f6b354cb6a87c90d28a92f11833fdac5
treatmesubj <jrock4503@hotmail.com>
2022-11-11 12:06:13 -0600

    kill mplayer works nicely

--- Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
+++ Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
@@ -8,3 +8,4 @@ sudo kill -9 $(pgrep fim)  # kill the fim process
 # ffmpeg & mplayer to watch videos
 ffmpeg -i input.mp4 -vf scale=240:240 output.mp4  # pi 0 w not beefy enough to do this itself. Takes forever
 sudo openvt -s -l -- mplayer -vo fbdev output.mp4
+sudo kill -9 $(pgrep mplayer)  # kill the fim process
