7ebb409d25ebb0e4ffa8953c1d0152e67cdaae4e
treatmesubj <jrock4503@hotmail.com>
2022-11-11 09:28:37 -0600

    alright, don't force

--- Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
+++ Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
@@ -1,6 +1,6 @@
 sudo fbi -a -vt 2 /mnt/usb_share/holding_big_gar.jpeg  # view image on tty2, if it exists
 sudo fbi -vt 2 -q  # quit fbi
 
-sudo openvt -c 2 -f -s -- fbi -a /mnt/usb_share/holding_big_gar.jpeg  # create and switch to tty2 just to view an image
+sudo openvt -c 2 -s -- fbi -a /mnt/usb_share/holding_big_gar.jpeg  # create and switch to tty2 just to view an image
 sudo chvt 1  # switch back to tty1
 # sudo kill - 9 <fbi's pid>  # kill the fbi process
