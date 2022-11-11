cc9838183d9ec5649877d45c0568bb0d339c0c9c
treatmesubj <jrock4503@hotmail.com>
2022-11-11 11:54:05 -0600

    hahaha it works

--- Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
+++ Linux/Raspberry_Pi_Stuff/display_image_on_tty.sh
@@ -1,6 +1,10 @@
-# fim is basically fib with more options
+# fim is basically fbi with more options
 sudo fbi -a -vt 2 /mnt/usb_share/holding_big_gar.jpeg  # view image on tty2, if it exists
 
 sudo openvt -s -l -- fim /mnt/usb_share/john_w_kirk.jpeg # create and switch to tty2 just to view an image
 sudo chvt 1  # switch back to tty1
-sudo kill -9 $(pgrep fim)  # kill the fbi process
+sudo kill -9 $(pgrep fim)  # kill the fim process
+
+# ffmpeg & mplayer to watch videos
+ffmpeg -i input.mp4 -vf scale=240:240 output.mp4  # pi 0 w not beefy enough to do this itself. Takes forever
+sudo openvt -s -l -- mplayer -vo fbdev output.mp4
