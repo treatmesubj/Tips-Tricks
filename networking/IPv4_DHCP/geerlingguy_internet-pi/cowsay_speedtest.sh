1807fefb351bf8596e59ce620441498e2c631ece
treatmesubj <jrock4503@hotmail.com>
2022-12-06 16:48:57 -0600

    cowsay speedtest

---
+++ networking/IPv4_DHCP/geerlingguy_internet-pi/cowsay_speedtest.sh
@@ -0,0 +1,6 @@
+while sleep 600; do \
+	curl -s http://127.0.0.1:9798/metrics | \
+	grep -E '^(speedtest_upload_bits_per_second|speedtest_download_bits_per_second)' | \
+	cowsay -f tux | \
+	lolcat --force --spread 1.0;
+done;
