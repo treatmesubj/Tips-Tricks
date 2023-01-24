0d8476f633fd8ba21fac24307f7ba6e980293393
treatmesubj <jrock4503@hotmail.com>
2023-01-24 14:33:28 -0600

    indentation

--- Python/parquets/parquet_to_csv.py
+++ Python/parquets/parquet_to_csv.py
@@ -4,28 +4,28 @@ import pandas as pd
 
 
 def parquet_to_csv(in_file, out_file=None):
-	"""
-	Creates a CSV file from parquet file.
-	If no desired resulting CSV file-path is given,
-	the CSV is created in the directory of the parquet file.
-	"""
-	df = pd.read_parquet(in_file, engine='fastparquet')
-	df.columns = df.columns.astype(str)
-	if out_file:
-		df.to_csv(out_file, index=False)
-		print(out_file)
-	else:
-		pre, ext = os.path.splitext(in_file)
-		out_file = f"{pre}.csv"
-		df.to_csv(out_file, index=False)
-		print(out_file)
+    """
+    Creates a CSV file from parquet file.
+    If no desired resulting CSV file-path is given,
+    the CSV is created in the directory of the parquet file.
+    """
+    df = pd.read_parquet(in_file, engine='fastparquet')
+    df.columns = df.columns.astype(str)
+    if out_file:
+        df.to_csv(out_file, index=False)
+        print(out_file)
+    else:
+        pre, ext = os.path.splitext(in_file)
+        out_file = f"{pre}.csv"
+        df.to_csv(out_file, index=False)
+        print(out_file)
         return df
 
 
 if __name__ == "__main__":
-	in_file = sys.argv[1]
-	try:
-		out_file = sys.argv[2]
-	except IndexError:
-		out_file = None
-	df = parquet_to_csv(in_file=in_file, out_file=out_file)
+    in_file = sys.argv[1]
+    try:
+        out_file = sys.argv[2]
+    except IndexError:
+        out_file = None
+    df = parquet_to_csv(in_file=in_file, out_file=out_file)
