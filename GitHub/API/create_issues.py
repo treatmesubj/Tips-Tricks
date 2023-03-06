a175b74d489e165645e85407ffba4b62a67ebc9a
treatmesubj <jrock4503@hotmail.com>
2023-03-06 17:29:23 -0600

    keyring

--- GitHub/API/create_issues.py
+++ GitHub/API/create_issues.py
@@ -8,7 +8,7 @@ import keyring.util.platform_ as keyring_platform
 
 
 def make_github_issue(
-        username, password,
+        username, password, keyringy,
         repo_owner, repo_name,
         title, body=None, labels=None
         ):
@@ -16,8 +16,8 @@ def make_github_issue(
     url = f"https://github.ibm.com/api/v3/repos/{repo_owner}/{repo_name}/issues"
     # Create an authenticated session to create the issue
     session = requests.Session()
-    if args.keyring:
-        session.auth(keyring.get_password(service_name="github.ibm.com", username=username))
+    if args.keyringy:
+        session.auth = (username, keyring.get_password(service_name="github.ibm.com", username=username))
     else:
         session.auth = (username, password)
     # Create our issue
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--username', '-u', required=True, action="store")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--keyring', '-k', default=None, action="store")
+    group.add_argument('--keyringy', '-k', default=None, action="store_true")
     group.add_argument('--password', '-p', default=None, action="store")
     parser.add_argument('--repo_owner', '-o', required=True, action="store")
     parser.add_argument('--repo_name', '-n', required=True, action="store")
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     make_github_issue(
-        args.username, args.password, args.keyring,
+        args.username, args.password, args.keyringy,
         args.repo_owner, args.repo_name,
         args.title, args.body, args.labels
         )
