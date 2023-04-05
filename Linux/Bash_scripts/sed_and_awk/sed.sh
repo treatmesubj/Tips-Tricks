5b8d75b9d516851ddeeaacffd8104847cda8ef38
treatmesubj <jrock4503@hotmail.com>
2023-04-05 14:58:28 -0500

    sed -e

--- Linux/Bash_scripts/sed_and_awk/sed.sh
+++ Linux/Bash_scripts/sed_and_awk/sed.sh
@@ -2,3 +2,5 @@ sed 's/to_change/changed/' infile.txt   # find and replace
 sed 's,to_change,changed,' infile.txt   # find and replace (comma delimeter instead of slash)
 sed 's/to_change//' infile.txt          # find and remove
 echo "to_change" | sed 's/to_change/changed/'
+
+sed -e 's/to_change/changed/' -e 's/changed/chaned_again/' infile.txt  # use multiple scripts
