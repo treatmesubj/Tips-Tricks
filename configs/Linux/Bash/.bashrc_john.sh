0d8a7efe78027e7823009b3df0ab3039495814f4
treatmesubj <jrock4503@hotmail.com>
2023-04-07 17:06:17 -0500

    bashrc

--- configs/Linux/Bash/.bashrc_john.sh
+++ configs/Linux/Bash/.bashrc_john.sh
@@ -1,10 +1,10 @@
 # echo  'source ~/.bashrc_john.sh' >> ~/.bashrc
 # echo  'source ~/.bashrc_john.sh' >> ~/.bash_profile
 # echo  'source ~/.bashrc_john.sh' >> ~/.bash_login
-export PS1="\[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]\h\[\e[m\]:\[\e[1;34m\]\w\[\e[m\]\r\n\[\e[1;32m\]\$\[\e[m\] \[$(tput sgr0)\]"
+# export PS1="\[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]\h\[\e[m\]:\[\e[1;34m\]\w\[\e[m\]\r\n\[\e[1;32m\]\$\[\e[m\] \[$(tput sgr0)\]"
 alias list='ls -a -h -s -1 --color'
 # alias thesr='python3 -m thesr.thesr'
-export TERM="xterm-256color"
+# export TERM="xterm-256color"
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 source ~/.venv/bin/activate #python venv
 # export winhome="/mnt/c/Users/JohnHupperts"
