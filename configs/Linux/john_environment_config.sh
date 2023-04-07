5d7d9d157a23be3dfd85b3eef90f0bc22ec0d366
treatmesubj <jrock4503@hotmail.com>
2023-04-07 16:29:29 -0500

    sh

--- configs/Linux/john_environment_config.sh
+++ configs/Linux/john_environment_config.sh
@@ -1,7 +1,12 @@
+# vimrc
 curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Vim/vimrc" -o ~/.vimrc
 python3 -m venv ~/.venv
+# tmux conf
 curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/TMUX/tmux.conf" -o ~/.tmux.conf
+# bashrc
 curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Bash/.bashrc_john.sh" -o ~/.bashrc_john.sh
+echo  'source ~/.bashrc_john.sh' >> ~/.bashrc
+# git config
 curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/git_stuff/gitignore.txt" -o ~/.gitignore
 git config --globalcore.excludesfile ~/.gitignore
 
