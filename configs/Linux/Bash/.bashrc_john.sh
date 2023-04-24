cc9656f38ea55f8e23998882b6d1f53089d6d1cd
treatmesubj <jrock4503@hotmail.com>
2023-04-24 15:07:05 -0500

    reorder

--- configs/Linux/Bash/.bashrc_john.sh
+++ configs/Linux/Bash/.bashrc_john.sh
@@ -1,17 +1,23 @@
 # append 'source ~/.bashrc_john.sh' to ~/.bashrc ~/.bash_profile  ~/.bash_login
 prompt() {
+    # user@host: ~
+    export PS1="\[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]\h\[\e[m\]:\[\e[1;34m\]\w\[\e[m\]\r\n";
+
+    # py-venv: /home/john/.venv
     if [[ $VIRTUAL_ENV ]]
     then
-        export PS1="\[\e[1;34m\]py-venv: \[\e[m\]\[\e[1;32m\](\$VIRTUAL_ENV)\[\e[m\]\r\n";
+        export PS1+="\[\e[1;34m\]py-venv: \[\e[m\]\[\e[1;32m\](\$VIRTUAL_ENV)\[\e[m\]\r\n";
     fi;
 
+    # branch: master
     branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d')
     if [[ ! -z "$branch" ]]
     then
        export PS1+="\[\e[1;34m\]branch: \[\e[m\]\[\e[1;32m\]$branch\[\e[m\]\r\n";
     fi;
 
-    export PS1+="\[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]\h\[\e[m\]:\[\e[1;34m\]\w\[\e[m\]\r\n\[\e[1;32m\]\$\[\e[m\] \[$(tput sgr0)\]";
+    # $
+    export PS1+="\[\e[1;92m\]\$\[\e[m\] \[$(tput sgr0)\]"
 }
 PROMPT_COMMAND=prompt
 alias list='ls -a -h -s -1 --color'
