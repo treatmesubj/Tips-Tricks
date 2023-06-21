242e7183f0e257eb74bd116d4d22932ce0d92d2c
treatmesubj <jrock4503@hotmail.com>
2023-06-21 10:56:32 -0500

    better

--- Python/pandas/column_max_lens.py
+++ Python/pandas/column_max_lens.py
@@ -1,6 +1,23 @@
 import pandas as pd
+import argparse
+import os
+# from pathlib import Path
 
 
-df = pd.read_excel('data.xlsx')
+parser = argparse.ArgumentParser()
+parser.add_argument("--excel_file", "-f", default=None, help="Excel file path")
+args = parser.parse_args()
+
+
+if args.excel_file and all(
+    (args.excel_file != "none", not os.path.isfile(args.excel_file))
+):
+    raise FileNotFoundError(
+        f"looks like the given `excel_file` argument's value - {args.excel_file} - cannot be found"
+    )
+
+
+
+df = pd.read_excel(args.excel_file, dtype=str)
 for col in df:
     print(col, "->", df[col].astype(str).str.len().max())
