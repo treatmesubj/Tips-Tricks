df16040704ad4f28746e6a81fbf8648538a362d9
treatmesubj <jrock4503@hotmail.com>
2023-06-21 17:27:35 -0500

    pyspark demo

---
+++ Apache_Spark/pyspark_jdbc.py
@@ -0,0 +1,84 @@
+from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
+import os
+
+
+def get_df(
+    spark_session,
+    fields: str,
+    schema_table: str,
+    jdbc_url: str,
+    user: str,
+    password: str,
+    driver: str = "com.ibm.db2.jcc.DB2Driver",
+    order_fields: str = None,
+    partitions: int = None,
+    where: str = None,
+    order_by: str = None,
+    fetch_first: str = None,
+):
+    statement = f"(select {fields} "
+
+    if order_fields and partitions:
+        statement += f", mod(rownumber() over(order by {order_fields}), {partitions}) mod_rn_partition_extra "
+
+    statement += f"from {schema_table} "
+
+    if where:
+        statement += f"{where} "
+
+    if order_by:
+        statement += f"{order_by} "
+
+    if fetch_first:
+        statement += f"{fetch_first} "
+
+    statement += ") as tab"
+    print(f"statement: {statement}")
+
+    df_config = (
+        spark_session.read.format("jdbc")
+        .option("url", jdbc_url)
+        .option("driver", driver)
+        .option("user", user)
+        .option("password", password)
+        .option("dbtable", statement)
+    )
+    if partitions:
+        df_config = (
+            df_config.option("partitionColumn", "mod_rn_partition_extra")
+            .option("lowerBound", 0)
+            .option("upperBound", partitions)
+            .option("numPartitions", partitions)
+            .option("fetchsize", "10000")
+        )
+    df = df_config.load()
+    if partitions:
+        df = df.drop("mod_rn_partition_extra")
+
+    return df
+
+
+if __name__ == "__main__":
+    conf = SparkConf()
+    conf.set("spark.jars", "/mnt/c/Users/JohnHupperts/Documents/JARs/db2jcc4.jar") 
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
+    df = get_df(
+        spark_session=spark,
+        fields="*",
+        schema_table="SYSIBM.SYSTABLES",
+        jdbc_url="jdbc:db2://db2w-host:50001/BLUDB:sslConnection=true;",
+        user=os.getenv("db_user"),
+        password=os.getenv("db_pw"),
+    )
+    df.createOrReplaceTempView("df")
+
+    sub_df = spark.sql(
+        """
+        SELECT *
+        FROM df
+        LIMIT 50;
+        """
+    )
+    print(sub_df.head())
