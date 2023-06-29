4c7a3e2b207a9ba12a8007b1edd1e91508fd49df
treatmesubj <jrock4503@hotmail.com>
2023-06-29 14:37:48 -0500

    proof

--- networking/Cisco_Anyconnect_VPN_WSL2.ps1
+++ networking/Cisco_Anyconnect_VPN_WSL2.ps1
@@ -5,6 +5,7 @@ Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | G
 (Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-DnsClientServerAddress).ServerAddresses # to be added to WSL /etc/resolv.conf
 # set Cisco VPN interfacemetric greater than WSL's so it is preferred - https://learn.microsoft.com/en-us/windows-server/networking/technologies/network-subsystem/net-sub-interface-metric
 Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Set-NetIPInterface -InterfaceMetric 6000
+Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected"
 
 # go back to normal state when done with WSL tunnelall stuff
 Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Where-Object AddressFamily -like "IPv6" | Set-NetIPInterface -InterfaceMetric 45
