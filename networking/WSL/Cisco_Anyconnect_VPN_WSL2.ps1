7d76c6e2d9e1f0fd1b6ad0a1b7af3744290eedd2
treatmesubj <jrock4503@hotmail.com>
2023-07-19 09:42:08 -0500

    updates

--- networking/WSL/Cisco_Anyconnect_VPN_WSL2.ps1
+++ networking/WSL/Cisco_Anyconnect_VPN_WSL2.ps1
@@ -1,4 +1,5 @@
 # connect Cisco Anyconnect VPN to TUNNELALL
+# Administrator PowerShell
 wsl --shutdown
 Get-NetIPInterface | Where-Object ConnectionState -like "Connected"
 Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected"
@@ -6,6 +7,15 @@ Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | G
 # set Cisco VPN interfacemetric greater than WSL's so it is preferred - https://learn.microsoft.com/en-us/windows-server/networking/technologies/network-subsystem/net-sub-interface-metric
 Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Set-NetIPInterface -InterfaceMetric 6000
 Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected"
+# start WSL
+# you may need to fix WSL's DNS too
+    # $ dig -4 w3.ibm.com # to check if DNS works
+    # if not, from PS, PS> ipconfig /all
+        # see Ethernet adapter with DNS Suffix ibm.com
+        # see DNS Servers
+            # add DNS Server to WSL's /etc/resolv.conf
+# $ sudo vim etc/resolv.conf
+
 
 # go back to normal state when done with WSL tunnelall stuff
 Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Where-Object AddressFamily -like "IPv6" | Set-NetIPInterface -InterfaceMetric 45
