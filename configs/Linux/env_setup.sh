bb6a962e59bc87d34d25902b6502097cdfda749f
treatmesubj <jrock4503@hotmail.com>
2023-08-11 15:07:06 -0500

    lfs reqs

--- configs/Linux/env_setup.sh
+++ configs/Linux/env_setup.sh
@@ -1,13 +1,13 @@
 # curl https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/env_setup.sh > env_setup.sh
 # vimrc
-sudo apt install vim \
+sudo apt install vim -y\
     && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Vim/vimrc" -o ~/.vimrc \
     && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # python venv
 sudo apt install python3-venv -y && python3 -m venv ~/.venv
 # tmux conf
-sudo apt install tmux && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/TMUX/tmux.conf" -o ~/.tmux.conf
+sudo apt install tmux -y && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/TMUX/tmux.conf" -o ~/.tmux.conf
 # bashrc
 curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Bash/.bashrc_john.sh" -o ~/.bashrc_john.sh
 echo  'source ~/.bashrc_john.sh' >> ~/.bashrc
@@ -15,7 +15,15 @@ echo  'source ~/.bashrc_john.sh' >> ~/.bash_profile
 echo  'source ~/.bashrc_john.sh' >> ~/.bash_login
 source ~/.bashrc_john.sh
 # git config
-sudo apt install git && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/git_stuff/gitignore.txt" -o ~/.gitignore
+sudo apt install git -y && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/git_stuff/gitignore.txt" -o ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
 
+# lfs requirements
+sudo apt install build-essential -y
+sudo ln -sf bash /bin/sh
+sudo apt install bison -y
+sudo apt install gawk -y
+sudo apt install texinfo -y
+
 printf "\nPlease, to finish Vim setup: '\$ vim ~/.vimrc', then ':PlugInstall'\n\n"
+
