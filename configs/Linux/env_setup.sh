bbbd188fb815bc1636056f1913ef1ae5bf18e74f
treatmesubj <jrock4503@hotmail.com>
2023-08-11 11:39:05 -0500

    env setup

--- configs/Linux/env_setup.sh
+++ configs/Linux/env_setup.sh
@@ -1,10 +1,13 @@
 # curl https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/env_setup.sh > env_setup.sh
 # vimrc
-apt install vim && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Vim/vimrc" -o ~/.vimrc
+sudo apt install vim\
+    && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Vim/vimrc" -o ~/.vimrc\
+    && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim\
 # python venv
-apt install python3-venv -y && python3 -m venv ~/.venv
+sudo apt install python3-venv -y && python3 -m venv ~/.venv
 # tmux conf
-apt install tmux && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/TMUX/tmux.conf" -o ~/.tmux.conf
+sudo apt install tmux && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/TMUX/tmux.conf" -o ~/.tmux.conf
 # bashrc
 curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Bash/.bashrc_john.sh" -o ~/.bashrc_john.sh
 echo  'source ~/.bashrc_john.sh' >> ~/.bashrc
@@ -12,5 +15,9 @@ echo  'source ~/.bashrc_john.sh' >> ~/.bash_profile
 echo  'source ~/.bashrc_john.sh' >> ~/.bash_login
 source ~/.bashrc_john.sh
 # git config
-apt install git && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/git_stuff/gitignore.txt" -o ~/.gitignore
+sudo apt install git && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/git_stuff/gitignore.txt" -o ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
+
+echo ""
+echo "please: '\$ vim ~/.vimrc', then ':PlugInstall'"
+echo ""
