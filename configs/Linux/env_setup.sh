b85704a4561b88f3385ab584a0b59385d1776d82
treatmesubj <jrock4503@hotmail.com>
2023-08-11 11:21:18 -0500

    try this

--- configs/Linux/env_setup.sh
+++ configs/Linux/env_setup.sh
@@ -1,16 +1,15 @@
 # vimrc
-curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Vim/vimrc" -o ~/.vimrc
+apt install vim && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Vim/vimrc" -o ~/.vimrc
 # python venv
-sudo apt install python3-venv -y
-python3 -m venv ~/.venv
+apt install python3-venv -y && python3 -m venv ~/.venv
 # tmux conf
-curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/TMUX/tmux.conf" -o ~/.tmux.conf
+apt install tmux && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/TMUX/tmux.conf" -o ~/.tmux.conf
 # bashrc
 curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Bash/.bashrc_john.sh" -o ~/.bashrc_john.sh
 echo  'source ~/.bashrc_john.sh' >> ~/.bashrc
 echo  'source ~/.bashrc_john.sh' >> ~/.bash_profile
 echo  'source ~/.bashrc_john.sh' >> ~/.bash_login
-source ~/.bashrc
+source ~/.bashrc_john.sh
 # git config
-curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/git_stuff/gitignore.txt" -o ~/.gitignore
+apt install git && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/git_stuff/gitignore.txt" -o ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
