1cc46a414ef8f3bde7297ead57f1a4f973257899
treatmesubj <jrock4503@hotmail.com>
2023-08-18 12:05:24 -0500

    db2 query audit

---
+++ SQL/DB2_Query_Audit.sql
@@ -0,0 +1,11 @@
+-- https://stackoverflow.com/questions/55495082/list-history-of-sql-statements-in-db2-warehouse-on-cloud
+-- https://www.ibm.com/docs/en/db2woc?topic=SS6NHC/com.ibm.swg.im.dashdb.security.doc/doc/audit_policy_guidelines.htm#audit_policy_guidelines__title__3
+
+create audit policy exec_policy categories execute status both error type normal;
+audit database using policy exec_policy;
+
+-- *user executes statement*
+
+call audit.update(); -- updates audit.execute table from logs
+
+select * from audit.execute
