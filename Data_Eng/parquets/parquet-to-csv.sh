9f862b2ac417d9b30f792f2ccc81e339f6f55ea4
treatmesubj <jrock4503@hotmail.com>
2023-08-18 09:59:44 -0500

    parquets

--- Data_Eng/parquets/parquet-to-csv.sh
+++ Data_Eng/parquets/parquet-to-csv.sh
@@ -1,3 +1,7 @@
+# See ./parquet-tools.md
+
 # https://github.com/hangxie/parquet-tools
-cat ./parquet-tools.md
 parquet-tools cat -f csv data.parquet > data.csv
+
+# https://github.com/ktrueda/parquet-tools
+parquet-tools csv data.parquet > data.csv
