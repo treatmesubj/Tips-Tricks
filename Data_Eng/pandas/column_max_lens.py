f145c6ceda7f38b63ab0541638d3ad408733c9d8
treatmesubj <jrock4503@hotmail.com>
2023-08-24 10:55:35 -0500

    double

--- Data_Eng/pandas/column_max_lens.py
+++ Data_Eng/pandas/column_max_lens.py
@@ -20,13 +20,14 @@ def round_power_of_2(n: int):
     return n << 1
 
 
-def nargs_req_len(nmin,nmax):
+def nargs_req_len(nmin, nmax):
     class RequiredLength(argparse.Action):
         def __call__(self, parser, args, values, option_string=None):
-            if not nmin<=len(values)<=nmax:
-                msg=f"argument '{self.dest}' requires between {nmin} and {nmax} arguments\n{self.help}"
+            if not nmin <= len(values) <= nmax:
+                msg = f"argument '{self.dest}' requires between {nmin} and {nmax} arguments\n{self.help}"
                 raise argparse.ArgumentTypeError(msg)
             setattr(args, self.dest, values)
+
     return RequiredLength
 
 
@@ -34,10 +35,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument(
-        "--excel_file", "-e", nargs='+', action=nargs_req_len(1,2), help="Excel file path & sheet name; {-e file.xlsx my_sheet}"
+        "--excel_file",
+        "-e",
+        nargs="+",
+        action=nargs_req_len(1, 2),
+        help="Excel file path & sheet name; {-e file.xlsx my_sheet}",
     )
-    group.add_argument("--csv_file", "-c", help="CSV file path")
-    group.add_argument("--parquet_file", "-p", help="parquet file path")
+    group.add_argument("--csv_file", "-c", action="store", help="CSV file path")
+    group.add_argument("--parquet_file", "-p", action="store", help="parquet file path")
+    parser.add_argument(
+        "--double", "-d", action="store_true", help="double VARCHAR length for testing"
+    )
     args = parser.parse_args()
 
     if args.excel_file:
@@ -47,7 +55,9 @@ if __name__ == "__main__":
                 f"looks like the given `excel_file` argument's value/path - {args.excel_file[0]} - cannot be found"
             )
         if len(args.excel_file) == 1:
-            args.excel_file += [None,]
+            args.excel_file += [
+                None,
+            ]
             print("no sheet name given; default to 1st sheet\n")
         df = pd.read_excel(args.excel_file[0], sheet_name=args.excel_file[1], dtype=str)
 
@@ -75,11 +85,13 @@ if __name__ == "__main__":
     print("#" * 50)
 
     # print approximate DDL
-    for col in df.columns[:-1]:
-        print(
-            f"{col.upper().replace('-', '_').replace(' ', '_')} VARCHAR({round_power_of_2(df[col].astype(str).str.len().max())}),"
-        )
-    # last col - no trailing comma
-    print(
-        f"{df.columns[-1].upper().replace('-', '_').replace(' ', '_')} VARCHAR({round_power_of_2(df.iloc[:,-1].astype(str).str.len().max())})"
-    )
+    for col in df.columns:
+        col_name = col.upper().replace("-", "_").replace(" ", "_")
+        length = round_power_of_2(df[col].astype(str).str.len().max())
+        if col == df.columns[-1]:
+            comma = ""
+        else:
+            comma = ","
+        if args.double:
+            length = length * 2
+        print(f"{col_name} VARCHAR({length}){comma}")
