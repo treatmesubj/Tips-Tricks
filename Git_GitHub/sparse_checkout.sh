7c62c21dec3eb1532c2f5490c20288dc2bd6d082
treatmesubj <jrock4503@hotmail.com>
2023-08-25 10:18:01 -0500

    even clearer

--- Git_GitHub/sparse_checkout.sh
+++ Git_GitHub/sparse_checkout.sh
@@ -7,5 +7,6 @@ git config core.sparseCheckout true
 
 echo "subdir/dir/dir" >> .git/info/sparse-checkout
 echo "another-subdir/dir/dir" >> .git/info/sparse-checkout
+# **NOTE**: repo's name is not 'top-level' dir in sparse-checkout path
 
 git pull origin <branch>
