edcc71b5cf50565aead965c2c6b079773ac3ac3f
treatmesubj <jrock4503@hotmail.com>
2023-09-12 11:27:08 -0500

    refactor

--- networking/WSL/Cisco_Anyconnect_VPN_WSL2.ps1
+++ networking/WSL/Cisco_Anyconnect_VPN_WSL2.ps1
@@ -1,21 +1,22 @@
 # connect Cisco Anyconnect VPN to TUNNELALL
 # Administrator PowerShell
-wsl --shutdown
 Get-NetIPInterface | Where-Object ConnectionState -like "Connected"
 Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected"
-(Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-DnsClientServerAddress).ServerAddresses # to be added to WSL /etc/resolv.conf
-# set Cisco VPN interfacemetric greater than WSL's so it is preferred - https://learn.microsoft.com/en-us/windows-server/networking/technologies/network-subsystem/net-sub-interface-metric
+
+# set Cisco VPN interfacemetric greater than WSL's so it is preferred
+# https://learn.microsoft.com/en-us/windows-server/networking/technologies/network-subsystem/net-sub-interface-metric
 Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Set-NetIPInterface -InterfaceMetric 6000
 Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected"
-# start WSL
-# you may need to fix WSL's DNS too
-    # $ dig -4 w3.ibm.com # to check if DNS works
-    # if not, from PS, PS> ipconfig /all
-        # see Ethernet adapter with DNS Suffix ibm.com
-            # see DNS Servers
-                # add IPv4 DNS Server to WSL's /etc/resolv.conf
-                # $ sudo vim etc/resolv.conf
 
+# VPN DNS servers to be added to WSL /etc/resolv.conf
+(Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-DnsClientServerAddress).ServerAddresses
+# start WSL
+# Add above DNS servers to /etc/resolv.conf
+# $ dig -4 w3.ibm.com # to check if DNS works
+# Troubleshooting
+# from PS, PS> ipconfig /all
+    # see Ethernet adapter with DNS Suffix ibm.com
+        # check if DNS Servers align with VPN's DNS Servers above
 
 # go back to normal state when done with WSL tunnelall stuff
 Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Where-Object AddressFamily -like "IPv6" | Set-NetIPInterface -InterfaceMetric 45
