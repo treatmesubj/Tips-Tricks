5af31324aa1131a384ba47034dfcfaea262f181b
treatmesubj <jrock4503@hotmail.com>
2023-09-13 11:41:08 -0500

    simplify

--- configs/PowerShell/Microsoft.PowerShell_profile.ps1
+++ configs/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -37,9 +37,7 @@ Set-PSReadlineKeyHandler -Key 'ctrl+l' -ScriptBlock {
 }
 
 function wsl_vpn {
-    Start-Process -FilePath powershell.exe -ArgumentList {
-        Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Set-NetIPInterface -InterfaceMetric 6000
-    } -verb RunAs
+    Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Set-NetIPInterface -InterfaceMetric 6000
 
     Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*"
     Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Format-Table -AutoSize
@@ -48,10 +46,8 @@ function wsl_vpn {
 }
 
 function reset_wsl_vpn {
-    Start-Process -FilePath powershell.exe -ArgumentList {
-        Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Where-Object AddressFamily -like "IPv6" | Set-NetIPInterface -InterfaceMetric 45
-        Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Where-Object AddressFamily -like "IPv4" | Set-NetIPInterface -InterfaceMetric 1
-    } -verb RunAs
+    Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Where-Object AddressFamily -like "IPv6" | Set-NetIPInterface -InterfaceMetric 45
+    Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Where-Object AddressFamily -like "IPv4" | Set-NetIPInterface -InterfaceMetric 1
 
     Get-NetAdapter | Where-Object InterfaceDescription -like "Cisco AnyConnect*" | Get-NetIPInterface | Where-Object ConnectionState -like "Connected" | Format-Table -AutoSize
 }
