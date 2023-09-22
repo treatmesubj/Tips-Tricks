a0d8e56bb90d028d160870c7a80e41fa08bc586d
treatmesubj <jrock4503@hotmail.com>
2023-09-22 16:09:55 -0500

    dns record types

---
+++ networking/DNS/record_types.sh
@@ -0,0 +1,6 @@
+dig A google.com  # ipv4
+dig AAAA google.com  # ipv6
+dig MX google.com  # mail exchange record
+dig SOA google.com  # primary name server, admin email, domain serial num
+
+dig * google.com  # everything known
