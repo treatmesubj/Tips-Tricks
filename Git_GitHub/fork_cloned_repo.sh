e439bd4b6c1cc24e4f6e28f826f910b5bcae780a
treatmesubj <jrock4503@hotmail.com>
2023-10-17 15:46:39 -0500

    fork cloned repo

---
+++ Git_GitHub/fork_cloned_repo.sh
@@ -0,0 +1,5 @@
+# this can be pretty slow
+git remote rename origin upstream
+# create new repo in GitHub
+git remote add origin git@github.com:<your-username>/<your-project>.git
+git push -u origin master
