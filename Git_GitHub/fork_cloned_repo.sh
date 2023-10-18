b75961c489633269d0fe4617accfbdcf6eabc8d7
treatmesubj <jrock4503@hotmail.com>
2023-10-18 08:28:25 -0500

    forking

--- Git_GitHub/fork_cloned_repo.sh
+++ Git_GitHub/fork_cloned_repo.sh
@@ -1,5 +1,14 @@
-# this can be pretty slow
+# Method 1
+# fork repo in GitHub and clone fork
+git remote add upstream git@github.com:<upstream-username>/<upstream-project>.git
+git remote -v
+git fetch upstream
+
+#---
+# Method 2
+# clone upstream repo
 git remote rename origin upstream
 # create new repo in GitHub
 git remote add origin git@github.com:<your-username>/<your-project>.git
 git push -u origin master
+
