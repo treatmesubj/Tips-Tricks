f95ab50e984d2422c575562e1166745165062d78
treatmesubj <jrock4503@hotmail.com>
2023-10-26 10:42:06 -0500

    git stuff

--- Git_GitHub/fork_repo.sh
+++ Git_GitHub/fork_repo.sh
@@ -16,7 +16,18 @@ git push -u origin master
 # pull latest upstream master, rebase local master commits on top, push to origin
 # just be aware that this will rewrite origin master's git log/history
 git add . && git commit -m wip
+# git log
 git pull --rebase upstream master
 # git log
 git push origin master --force
+
+#---
+# work on an upstream branch too
+git fetch upstream
+git checkout johnh
 # git log
+git pull --rebase upstream master
+# git log
+# <make some changes>
+git push upstream johnh --force
+
---
+++ Git_GitHub/simple_rebase.sh
@@ -0,0 +1,9 @@
+git checkout my-branch
+# <do stuff>
+git add .
+git commit -m wip
+# git log
+git pull --rebase origin master
+# git log
+git push origin my-branch --force
+
