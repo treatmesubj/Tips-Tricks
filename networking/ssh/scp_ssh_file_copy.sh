38694f6747fd7ef5ef966a9408346394be3c237a
treatmesubj <jrock4503@hotmail.com>
2023-10-27 11:40:01 -0500

    syntax hl

--- networking/ssh/scp_ssh_file_copy.sh
+++ networking/ssh/scp_ssh_file_copy.sh
@@ -1,6 +1,6 @@
-scp -r user@remote:/root/stuff .
 # creates a local `./stuff/` directory for the files
 # does not preserve symlinks! They're just normal files!
+scp -r user@remote:/root/stuff .
 
-scp ./my_file user@remote:/root/my_file
 # copies file from local to remote
+scp ./my_file user@remote:/root/my_file
