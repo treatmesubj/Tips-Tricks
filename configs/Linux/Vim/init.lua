adf11aaaac67414eb2e4f9781f30a19593aa16a6
treatmesubj <jrock4503@hotmail.com>
2023-11-10 16:51:23 -0600

    file path

--- configs/Linux/Vim/init.lua
+++ configs/Linux/Vim/init.lua
@@ -1,3 +1,4 @@
+-- ~/.config/nvim/init.lua
 vim.api.nvim_command('set runtimepath^=~/.vim runtimepath+=~/.vim/after')
 vim.api.nvim_command('let &packpath = &runtimepath')
 vim.api.nvim_command('source ~/.vimrc')
