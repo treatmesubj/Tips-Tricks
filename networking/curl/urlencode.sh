a1e3efa1a7f177ae049c4f4696cd7657d417c0a8
treatmesubj <jrock4503@hotmail.com>
2023-11-14 11:37:56 -0600

    -v

--- networking/curl/urlencode.sh
+++ networking/curl/urlencode.sh
@@ -1,11 +1,13 @@
 # use basic auth to execute a GET request with url encoded query paramaters
-curl --get -u $user:$pass "https://my.api.com/stuff"\
+# -v will echo full encoded URL
+curl -v --get -u $user:$pass "https://my.api.com/stuff"\
     --data-urlencode "param1={cool: sweet}"\
     --data-urlencode "param2=nice"\
     | jq > output.json
 
 # execute a POST request with url encoded data
-curl "https://my.api.com/stuff"\
+# -v will echo full encoded URL
+curl -v "https://my.api.com/stuff"\
     --data-urlencode "param1={cool: sweet}"\
     --data-urlencode "param2=nice"\
     | jq > output.json
