978c0e042220a5d30dfae28b8a8e31c45ed15ef8
treatmesubj <jrock4503@hotmail.com>
2023-11-29 17:26:43 -0600

    comments

--- Git_GitHub/git-rebase-squash.sh
+++ Git_GitHub/git-rebase-squash.sh
@@ -2,3 +2,11 @@
 git rebase -i HEAD~10
 # change all your terrible commits from <pick> to <squash>
     # and they will be squashed UP into the prior picked commit
+
+# if you're on a non-master branch
+# you also might want to...
+git pull -r origin master
+
+# and if you've pushed to your branch
+# you might need to re-write its history
+git push --force
