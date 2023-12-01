30940dcbbec242bbe2a5515c4d7fee57d541c940
treatmesubj <jrock4503@hotmail.com>
2023-12-01 15:37:21 -0600

    comments

--- Git_GitHub/rebase/simple-rebase.sh
+++ Git_GitHub/rebase/simple-rebase.sh
@@ -4,6 +4,7 @@ git add .
 git commit -m wip
 # git log
 git pull --rebase origin master
+# git checkout master && git pull & git checkout my-branch && git merge master
 # git log
 git push origin my-branch --force
 
