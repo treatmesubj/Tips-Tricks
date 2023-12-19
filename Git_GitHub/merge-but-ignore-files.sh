cf71bccafc31e404aeca92484ca245b2446fbbfd
treatmesubj <jrock4503@hotmail.com>
2023-12-19 10:25:10 -0600

    merge-but-ignore-files

---
+++ Git_GitHub/merge-but-ignore-files.sh
@@ -0,0 +1,6 @@
+git checkout main
+git merge --no-ff --no-commit feat-branch  # merge branch without creating a commit
+git reset HEAD file-to-ignore.txt  # un-stage file
+git checkout -- file-to-ignore.txt  # discard any changes made by the merge
+# or if new file to branch, just do: rm file-to-ignore.txt
+git commit -m "merged feat-branch"
