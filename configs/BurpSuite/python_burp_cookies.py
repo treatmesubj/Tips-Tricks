4dbe61c6b2c9557423faf1581c963465734846f4
treatmesubj <jrock4503@hotmail.com>
2023-12-29 11:41:34 -0600

    format

--- configs/BurpSuite/python_burp_cookies.py
+++ configs/BurpSuite/python_burp_cookies.py
@@ -11,22 +11,21 @@ if __name__ == "__main__":
     # openssl x509 -inform der -in certificate.cer -out certificate.pem
     # cookiejar = browser_cookie3.load()
     cookiejar = browser_cookie3.firefox()
-    #cookiejar = browser_cookie3.firefox(
+    # cookiejar = browser_cookie3.firefox(
     #    cookie_file="/mnt/c/Users/JohnHupperts/AppData/Roaming/Mozilla/Firefox/Profiles/zpg5udte.default-release/cookies.sqlite"
-    #)
+    # )
 
     proxies = {  # burpsuite
-        'http': 'http://127.0.0.1:8080',
-        'https': 'http://127.0.0.1:8080',
-        #'http': 'http://172.31.48.1:6666',  # from wsl
-        #'https': 'https://172.31.48.1:6666',  # from wsl
+        "http": "http://127.0.0.1:8080",
+        "https": "http://127.0.0.1:8080",
+        # 'http': 'http://172.31.48.1:6666',  # from wsl
+        # 'https': 'https://172.31.48.1:6666',  # from wsl
     }
 
     response_jobs = requests.get(
         "https://www.linkedin.com/jobs/search/?keywords=&location=United States&f_TPR=&f_WT=2",
         cookies=cookiejar,
         allow_redirects=True,
-#        proxies=proxies,
-#        verify="./cacert.pem"
+        #        proxies=proxies,
+        #        verify="./cacert.pem"
     )
-
