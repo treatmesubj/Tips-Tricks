34a2584236b0bfb2bbbfc71956d4999082215db8
treatmesubj <jrock4503@hotmail.com>
2024-01-24 12:19:20 -0600

    diffin

--- Git_GitHub/git-diff.sh
+++ Git_GitHub/git-diff.sh
@@ -5,3 +5,8 @@ git diff HEAD~1 HEAD
 # see just changed file names
 git diff --stat HEAD~1 HEAD
 git diff --name-only
+
+# see staged, uncommitted files' changes
+git add .
+git diff --cached
+git diff --staged
