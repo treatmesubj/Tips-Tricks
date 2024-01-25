905a11b9d66f985ea2305620ee461130872e281a
treatmesubj <jrock4503@hotmail.com>
2024-01-25 08:27:45 -0600

    find w regex

--- Linux/file-finding/find-file.sh
+++ Linux/file-finding/find-file.sh
@@ -1,3 +1,8 @@
-find ./ -type f -name *.conf -exec ls -al {} \; 2>/dev/null
+find ./ -type f -name *.conf
+find ./ -type f -regex '.*\.conf'
+find / -type f -name *.conf -newermt 2020-03-03
 
+
+find ./ -type f -name *.conf -exec ls -al {} \; 2>/dev/null
+find ./ -type f --regex '.*\.conf' -exec ls -al {} \; 2>/dev/null
 find / -type f -name *.conf -newermt 2020-03-03 -exec ls -al {} \; 2>/dev/null
