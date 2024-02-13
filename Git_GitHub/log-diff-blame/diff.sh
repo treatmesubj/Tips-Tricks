bfbdaba19dd7ec28f270e80884966ff212c5961b
treatmesubj <jrock4503@hotmail.com>
2024-02-13 08:58:48 -0600

    only diff some

--- Git_GitHub/log-diff-blame/diff.sh
+++ Git_GitHub/log-diff-blame/diff.sh
@@ -6,6 +6,8 @@ git diff HEAD~1 HEAD
 git diff --stat HEAD~1 HEAD
 git diff --name-only
 
+# only show certain file(s) in git diff
+git diff 154c4b9 HEAD -- ./epmingestion
 # ignore file(s) in git diff; don't show their changes
 git diff 44a65e HEAD -- ':!./pipelines/drafts/*'
 
