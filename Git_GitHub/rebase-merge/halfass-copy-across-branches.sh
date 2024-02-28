734f334bac3ddb55930c63942b42217e59cb0d6f
treatmesubj <jrock4503@hotmail.com>
2024-02-28 12:14:40 -0600

    docs

--- Git_GitHub/rebase-merge/halfass-copy-across-branches.sh
+++ Git_GitHub/rebase-merge/halfass-copy-across-branches.sh
@@ -1,9 +1,11 @@
 # https://stackoverflow.com/a/25749155
-# copy in files from another branch without a legitimate merge of commit history
+# copy in files from another branch (rather than doing a legitimate merge of
+# commit history & files)
+# WILL OVERWRITE FILES in working directory
 git checkout feat1
-git checkout feat2 special-dir/special-file  # simply copies in file from that branch
-git add special-dir/special-file
-git commit -m 'halfass merge of feat2'
+git checkout feat2 special-dir/special-file  # simply copy in file from that branch & stage it
+git status
+git commit -m 'halfass copy/merge of feat2'
 
 # if you're going to merge feat2 into feat1 later, the files you copied will be
 # no different, so there is no loss of commit history for those files anyway, so
