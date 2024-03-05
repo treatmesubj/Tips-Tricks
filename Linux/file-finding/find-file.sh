ca7b1c6a9721a961e01e8bc159c3fb57e37c9b72
treatmesubj <jrock4503@hotmail.com>
2024-03-05 15:17:48 -0600

    quotes

--- Linux/file-finding/find-file.sh
+++ Linux/file-finding/find-file.sh
@@ -1,9 +1,9 @@
-find ./ -type f -name *.conf
+find ./ -type f -name '*.conf'
 find ./ -type f -regex '.*\.conf'
 find ./ -type f -iregex '.*\.CoNf'  # case insensitive
-find / -type f -name *.conf -newermt 2020-03-03
+find / -type f -name '*.conf' -newermt 2020-03-03
 
 
-find ./ -type f -name *.conf -exec ls -al {} \; 2>/dev/null
+find ./ -type f -name '*.conf' -exec ls -al {} \; 2>/dev/null
 find ./ -type f --regex '.*\.conf' -exec ls -al {} \; 2>/dev/null
-find / -type f -name *.conf -newermt 2020-03-03 -exec ls -al {} \; 2>/dev/null
+find / -type f -name '*.conf' -newermt 2020-03-03 -exec ls -al {} \; 2>/dev/null
