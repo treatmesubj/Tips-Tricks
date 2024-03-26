19603f57b369588c4d1f6d9ff031ba7e5ecdde8d
treatmesubj <jrock4503@hotmail.com>
2024-03-26 09:00:59 -0500

    log on 1 file

--- Git_GitHub/log-diff-blame/log.sh
+++ Git_GitHub/log-diff-blame/log.sh
@@ -5,6 +5,8 @@ git log --graph --pretty=oneline --abbrev-commit
 git log path/to/file
 # log of commits pertaining to certain lines in file
 git log -L 14,26:path/to/file
+# log of commits pertaining to file, with diffs
+git log -p -- filename my-file.txt
 
 # log of commits where regex string in it
 git log -S regex -p
