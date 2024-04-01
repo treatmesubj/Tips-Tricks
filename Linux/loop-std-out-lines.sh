bf50f0ec468b4f0acdd49e1b2319ba07deea3843
treatmesubj <jrock4503@hotmail.com>
2024-04-01 11:50:52 -0500

    rename files

--- Linux/loop-std-out-lines.sh
+++ Linux/loop-std-out-lines.sh
@@ -2,4 +2,5 @@ ls -1 | while read line; do echo $line; done
 
 # git log | grep 'commit ' | cut -d ' ' -f2 | while read line; do git show $line; done | grep "secret"
 
-
+# rename files (file-123 -> 123)
+ls -1 | while read filename; do num=$(echo $filename | cut -d '-' -f2) && mv $filename $num; done
