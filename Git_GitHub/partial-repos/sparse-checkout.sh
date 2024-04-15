5d1f06fdeee9f555327d91211317a61717d87456
treatmesubj <jrock4503@hotmail.com>
2024-04-15 17:04:01 -0500

    sparse checkout monorepo

--- Git_GitHub/partial-repos/sparse-checkout.sh
+++ Git_GitHub/partial-repos/sparse-checkout.sh
@@ -1,13 +1,16 @@
 mkdir <repo>
 cd <repo>
 git init
-git remote add -f origin <.git url>
 
 git config core.sparseCheckout true
-
 echo "subdir/dir/dir" >> .git/info/sparse-checkout
 echo "another-subdir/dir/dir" >> .git/info/sparse-checkout
 # **NOTE**: repo's name is not 'top-level' dir in sparse-checkout path
 
-git pull origin <branch>
+git remote add origin <.git url>
+# git fetch --depth 1 origin master  # for massive monorepos
+git fetch origin master
+git checkout master
+
 git status
+git log
