1f52aedbb4fbe5abfbe716ea918b42c3719601be
treatmesubj <jrock4503@hotmail.com>
2024-04-19 11:07:52 -0500

    examples

---
+++ REGEX/examples.sh
@@ -0,0 +1,13 @@
+# grep for any SCHEMA.TABLE
+rg -i '\w+\.\w+\s'
+
+#   Show all lines that do not contain the # character.
+grep -v -E "#" /etc/ssh/sshd_config
+#   Search for all lines that contain a word that starts with Permit.
+grep -E "[.]*\sPermit" /etc/ssh/sshd_config
+#   Search for all lines that contain a word ending with Authentication.
+grep -E "Authentication\b" /etc/ssh/sshd_config
+#   Search for all lines containing the word Key.
+grep -E "key" /etc/ssh/sshd_config
+#   Search for all lines beginning with Password and containing yes.
+grep -E "^Password.*yes$" /etc/ssh/sshd_config
