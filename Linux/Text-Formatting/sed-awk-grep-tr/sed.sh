75d6e09af96c82943691e4327fbbebcf57b07219
treatmesubj <jrock4503@hotmail.com>
2024-05-01 17:16:11 -0500

    tabs to commas

--- Linux/Text-Formatting/sed-awk-grep-tr/sed.sh
+++ Linux/Text-Formatting/sed-awk-grep-tr/sed.sh
@@ -1,5 +1,5 @@
 sed 's/to_change/changed/' infile.txt   # find and replace
-sed 's/to_change/changed/g' infile.txt   # find and replace all occurances on each line
+sed 's/\t/,/g' infile.txt > outfile.csv   # find and replace all occurances of tabs w/ commas on each line
 sed -i 's/to_change/changed/g' infile.txt   # find and replace all occurances on each line and change file in-place
 sed 's,to_change,changed,' infile.txt   # find and replace (comma delimeter instead of slash)
 sed 's/to_change//' infile.txt          # find and remove
