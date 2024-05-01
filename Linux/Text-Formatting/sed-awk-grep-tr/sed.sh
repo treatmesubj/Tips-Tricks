16a78e754622d7d9e833d8e225458a0e4cb40bf4
treatmesubj <jrock4503@hotmail.com>
2024-05-01 17:06:22 -0500

    sed tricks

--- Linux/Text-Formatting/sed-awk-grep-tr/sed.sh
+++ Linux/Text-Formatting/sed-awk-grep-tr/sed.sh
@@ -1,4 +1,6 @@
 sed 's/to_change/changed/' infile.txt   # find and replace
+sed 's/to_change/changed/g' infile.txt   # find and replace all occurances on each line
+sed -i 's/to_change/changed/g' infile.txt   # find and replace all occurances on each line and change file in-place
 sed 's,to_change,changed,' infile.txt   # find and replace (comma delimeter instead of slash)
 sed 's/to_change//' infile.txt          # find and remove
 echo "to_change" | sed 's/to_change/changed/'
