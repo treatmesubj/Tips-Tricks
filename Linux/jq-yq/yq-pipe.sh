330deb8751f5c83fd0336cb13f10254fd7950887
treatmesubj <jrock4503@hotmail.com>
2024-05-07 10:20:10 -0500

    select

--- Linux/jq-yq/yq-pipe.sh
+++ Linux/jq-yq/yq-pipe.sh
@@ -1,3 +1,6 @@
 # https://mikefarah.gitbook.io/yq/operators/pipe
 # you can loop over entries in an array like below with pipe
 yq '.subdags | .[] | .id' dags/deployable/maindags/facts/dag-main-headcount-monthly.yaml
+
+# select a subset of yaml based on key-value, then pipe that to keep working
+yq '.spec.templates | .[] | .extract | .[] | .connections | .[] | select (.connector == "taxonomy") | .query' facts/headcount/eid/templates/headcount-monthly-eid-template.yaml
