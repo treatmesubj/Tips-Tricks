e1074f04da474adea1b97f201f90e3abb6dda2dd
treatmesubj <jrock4503@hotmail.com>
2024-05-30 11:58:20 -0500

    parquet partition cols

--- Data_Eng/parquets/conversion-csv-parquet.py
+++ Data_Eng/parquets/conversion-csv-parquet.py
@@ -21,7 +21,7 @@ def parquet_to_csv(in_file, out_file):
     return df
 
 
-def csv_to_parquet(in_file, out_file):
+def csv_to_parquet(in_file, out_file, partition_cols=None):
     """
     Creates a parquet file from CSV file.
     If no desired resulting parquet file-path is given,
@@ -30,7 +30,7 @@ def csv_to_parquet(in_file, out_file):
     df = pd.read_csv(in_file)
     print(df.head())
     df.columns = df.columns.astype(str)
-    df.to_parquet(out_file, engine="fastparquet")
+    df.to_parquet(out_file, engine="fastparquet", partition_cols=partition_cols)
     print(out_file)
 
     # parquet_schema = pa.Table.from_pandas(df=df).schema
@@ -46,11 +46,12 @@ def csv_to_parquet(in_file, out_file):
 
 
 def print_df_column_lengths(df):
+    print("string col max lens")
     for col in df.columns:
         try:
             print(f"{col}: {df[col].str.len().max()}")
         except AttributeError:
-            print(col)
+            print(f"{col}: -")
 
 
 if __name__ == "__main__":
@@ -59,6 +60,12 @@ if __name__ == "__main__":
     direction_group.add_argument("--csv-to-parquet", "-cp", action="store_true")
     direction_group.add_argument("--parquet-to-csv", "-pc", action="store_true")
     parser.add_argument("--in-file", "-i", action="store", required=True)
+    parser.add_argument(
+        "--parquet-partitions",
+        "-pp",
+        action="store",
+        help='comma separated list, e.g. "YEAR, QUARTER, MONTH"',
+    )
     parser.add_argument("--out-file", "-o", action="store")
     args = parser.parse_args()
 
@@ -72,7 +79,12 @@ if __name__ == "__main__":
         out_file = args.out_file
 
     if args.csv_to_parquet:
-        df = csv_to_parquet(in_file=args.in_file, out_file=out_file)
+        if args.parquet_partitions:
+            partition_cols = [x.strip() for x in args.parquet_partitions.split(",")]
+            print(f"{partition_cols=}")
+        df = csv_to_parquet(
+            in_file=args.in_file, out_file=out_file, partition_cols=partition_cols
+        )
     if args.parquet_to_csv:
         df = parquet_to_csv(in_file=args.in_file, out_file=out_file)
 
