d9e3023d1b5328290b7ba955a842a3c3ad566218
treatmesubj <jrock4503@hotmail.com>
2024-08-29 12:28:35 -0500

    refactor

--- configs/Linux/Vim/nvim/lua/Duckdb.lua
+++ configs/Linux/Vim/nvim/lua/Duckdb.lua
@@ -4,36 +4,20 @@
 
 local function duckdb(args)
   local range = args.range
-  local lines
-  if range == 0 then
-    lines = {}
+  local line1 = args.line1 - 1
+  local line2 = args.line2
+  line2 = line1 == line2 and line1 + 1 or line2
+  local stdin = vim.api.nvim_buf_get_lines(0, line1, line2, false)
+
+  if range ~= 0 then
+    local output = vim.system({'bash', '-c', 'duckdb -csv'}, {text=true, stdin=stdin}):wait()
+    local new_buf = vim.api.nvim_create_buf(true, false)
+    vim.api.nvim_buf_set_lines(new_buf, 0, -1, false, vim.fn.split(output.stdout, "\n"))
+    vim.api.nvim_set_option_value("ft", "csv", { buf = new_buf})
+    local win = vim.api.nvim_open_win(new_buf, true, {vertical = false,})
   else
-    local line1 = args.line1 - 1
-    local line2 = args.line2
-    line2 = line1 == line2 and line1 + 1 or line2
-    lines = vim.api.nvim_buf_get_lines(0, line1, line2, false)
+    print('no stdin')
   end
-  -- print(lines)
-
-  local stdin = nil
-  if #lines ~= 0 then
-    stdin = lines
-  end
-
-  local new_buf = vim.api.nvim_create_buf(true, false)
-  local win = vim.api.nvim_open_win(new_buf, true, {vertical = false,})
-
-  local on_exit = function(obj)
-    print(obj.code)
-    print(obj.signal)
-    print(obj.stdout)
-    print(obj.stderr)
-  end
-
-  local output = vim.system({'bash', '-c', 'duckdb -csv'}, {text=true, stdin=stdin}, on_exit):wait()
-
-  vim.api.nvim_buf_set_lines(new_buf, 0, -1, false, vim.fn.split(output.stdout, "\n"))
-  vim.api.nvim_set_option_value("ft", "csv", { buf = new_buf})
 end
 
 vim.api.nvim_create_user_command("Duckdb", duckdb, {
