ed868f32af5927b38fd89eada57347db1c0797a5
treatmesubj <jrock4503@hotmail.com>
2024-11-25 15:42:09 -0600

    rm plug from udp8

--- configs/Linux/env-update.sh
+++ configs/Linux/env-update.sh
@@ -17,14 +17,12 @@ function update_prompt {
 # vim
 if [[ 'y' = $(update_prompt "vimrc & nvim config") ]]; then
     cp ~/.vimrc ~/.vimrc.bak &&\
-    cp ~/.config/nvim/init.lua ~/.config/nvim/init.lua.bak \
-    && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Vim/.vimrc" \
-        -o ~/.vimrc \
-    && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" \
-    && curl -fLo ~/.config/nvim/init.lua --create-dirs \
-        "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Vim/nvim/init.lua" \
-    && curl -fLo ~/.config/nvim/lua/Duckdb.lua --create-dirs \
+    cp ~/.config/nvim/init.lua ~/.config/nvim/init.lua.bak\
+    && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Vim/.vimrc"\
+        -o ~/.vimrc\
+    && curl -fLo ~/.config/nvim/init.lua --create-dirs\
+        "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Vim/nvim/init.lua"\
+    && curl -fLo ~/.config/nvim/lua/Duckdb.lua --create-dirs\
         "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Vim/nvim/lua/Duckdb.lua"
     printf "\nPlease, to finish Vim setup: '\$ vim ~/.vimrc', then ':PlugInstall'"
     printf "\nPlease, to finish Neovim setup: '\$ nvim ~/.vimrc', then ':PlugInstall', then ':UpdateRemotePlugins'\n\n"
@@ -38,32 +36,32 @@ fi
 # ripgrep
 if [[ 'y' = $(update_prompt "ripgreprc") ]]; then
     cp ~/.ripgreprc ~/.ripgreprc.bak &&\
-    curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/ripgrep/.ripgreprc" \
+    curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/ripgrep/.ripgreprc"\
         -o ~/.ripgreprc
 fi
 # bashrc
 if [[ 'y' = $(update_prompt "bashrc") ]]; then
-    cp ~/.bashrc_john.sh ~/.bashrc_john.sh.bak  &&\
-    curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Bash/.bashrc_john.sh" \
-        -o ~/.bashrc_john.sh \
-    && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Bash/work-env/.bashrc_cirrus.sh" \
-        -o ~/.bashrc_cirrus.sh \
+    cp ~/.bashrc_john.sh ~/.bashrc_john.sh.bak &&\
+    curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Bash/.bashrc_john.sh"\
+        -o ~/.bashrc_john.sh\
+    && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Bash/work-env/.bashrc_cirrus.sh"\
+        -o ~/.bashrc_cirrus.sh\
     && curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Bash/work-env/.bashrc_cluster.sh" \
         -o ~/.bashrc_cluster.sh
     source ~/.bashrc_john.sh
 fi
 # inputrc
 if [[ 'y' = $(update_prompt "inputrc") ]]; then
-    cp ~/.inputrc ~/.inputrc.sh.bak  &&\
-    curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Bash/.inputrc" \
+    cp ~/.inputrc ~/.inputrc.sh.bak &&\
+    curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Linux/Bash/.inputrc"\
         -o ~/.inputrc
 fi
 # git
 if [[ 'y' = $(update_prompt "gitignore & gitconfig") ]]; then
-    cp ~/.gitignore ~/.gitignore.bak  &&\
-    curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Git/.gitignore" \
+    cp ~/.gitignore ~/.gitignore.bak &&\
+    curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Git/.gitignore"\
         -o ~/.gitignore
-    cp ~/.gitconfig ~/.gitconfig.bak  &&\
-    curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Git/.gitconfig" \
+    cp ~/.gitconfig ~/.gitconfig.bak &&\
+    curl "https://raw.githubusercontent.com/treatmesubj/Tips-Tricks/master/configs/Git/.gitconfig"\
         -o ~/.gitconfig
 fi
