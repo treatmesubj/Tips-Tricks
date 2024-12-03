4fb9480c8143192dd29236daa6a2f9379525e80d
treatmesubj <jrock4503@hotmail.com>
2024-12-03 08:55:32 -0600

    openssl

--- networking/openssl_connection.sh
+++ networking/openssl_connection.sh
@@ -1,1 +1,2 @@
-openssl s_client -showcerts -connect <host>:<port> -servername <host> -CAfile <file.cert>
+openssl s_client -connect <host>:<port> -showcerts # -servername <host> -CAfile <file.cert>
+openssl s_client -connect <host>:<port> -showcerts # -servername <host> -CAfile <(keytool -list -rfc -keystore <ibm-truststore.jks> -storepass <TrustStore-password>)
