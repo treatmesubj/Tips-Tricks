fce95679ab037c181f0a818ffcef8509467c367a
treatmesubj <jrock4503@hotmail.com>
2024-12-13 12:09:54 -0600

    cherry-pick

---
+++ Git_GitHub/rebase-merge/cherry-pick-commit.sh
@@ -0,0 +1,7 @@
+# take a commit from somewhere and play it on HEAD as new commit
+git cherry-pick <hash>
+# Auto-merging <file>
+# Auto-merging <file>
+# Auto-merging <file>
+# Auto-merging <file>
+# Auto-merging <file>
