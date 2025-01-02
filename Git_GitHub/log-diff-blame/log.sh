7154db6c31a49fe2b2a0a38a02edc60a713667e2
treatmesubj <jrock4503@hotmail.com>
2025-01-02 09:03:50 -0600

    regex

--- Git_GitHub/log-diff-blame/log.sh
+++ Git_GitHub/log-diff-blame/log.sh
@@ -4,23 +4,26 @@ git log --pretty=\"format:%C(yellow)%h %C(Cyan)%>(10)%ad (%cr) %Cgreen%aN%Cred%d
 # Ascii Tree
 git log --graph --pretty=oneline --abbrev-commit
 
-# log of commits pertaining to file
+# pertaining to file
 git log path/to/file
-# log of commits pertaining to certain lines in file
+# pertaining to certain lines in file
 git log -L 14,26:path/to/file
 
 # log of commits, with diffs, with merges diff'd as 1 commit
 git log -p --first-parent -m
 
-# log of commits pertaining to file, with diffs
+# pertaining to file, with diffs
 git log -p -- filename my-file.txt
 
 # only show file names changed relative to cwd
 git log --name-only --relative
 
-# log of commits where regex string in it
-git log -G regex # https://git-scm.com/docs/git-log#Documentation/git-log.txt--Gltregexgt
-git log -S regex -p # https://git-scm.com/docs/git-log#Documentation/git-log.txt--Sltstringgt
+# where regex string in commit diff
+git log -p -G regex # https://git-scm.com/docs/git-log#Documentation/git-log.txt--Gltregexgt
+git log -p -S string # https://git-scm.com/docs/git-log#Documentation/git-log.txt--Sltstringgt
+
+# where regex in commit message
+git log -p --grep=regex
 
 # log across all branches by author
 git log --all --stat --author="John.Hupperts@ibm.com"
