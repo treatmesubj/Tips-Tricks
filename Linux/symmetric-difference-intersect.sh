d6fe4f5c509dd8864b6cf585836f2c332a9e84e3
treatmesubj <jrock4503@hotmail.com>
2025-02-21 11:57:18 -0600

    symm diff

--- Linux/symmetric-difference-intersect.sh
+++ Linux/symmetric-difference-intersect.sh
@@ -1,5 +1,7 @@
 # intersection of two lists
 sort <(ls one) <(ls two) | uniq -d
+sort file-1.txt file-2.txt | uniq -d
 
 # symmetric difference of two lists
 sort <(ls one) <(ls two) | uniq -u
+sort file-1.txt file-2.txt | uniq -u
