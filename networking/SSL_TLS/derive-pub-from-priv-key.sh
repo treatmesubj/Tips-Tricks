4769ed0bb9d4d46fcdd99d5b0839a99319168f5e
treatmesubj <jrock4503@hotmail.com>
2025-02-26 15:31:39 -0600

    priv vs pub encryption

--- networking/SSL_TLS/derive-pub-from-priv-key.sh
+++ networking/SSL_TLS/derive-pub-from-priv-key.sh
@@ -1,2 +1,12 @@
 # https://stackoverflow.com/a/1373088
 openssl rsa -in private.pem -pubout -out public.pem
+
+# https://security.stackexchange.com/questions/9957/can-i-use-a-private-key-as-a-public-key-and-vice-versa/9964#9964
+# encrypt with pub or priv key?
+
+# 1. if you wanted to put encrypted data out in the world in a Cloudant doc that
+# only you could decrypt with your unshared private key, you'd do it that way;
+# encrypt with public, decrypt with private
+
+# 2. if you want to prove data came from you, you'd encrypt with private, since
+# only you could encrypt that way
