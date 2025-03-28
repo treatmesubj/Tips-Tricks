4f22000718e28929c9a043b5cdc660f654c30e54
treatmesubj <jrock4503@hotmail.com>
2025-03-28 14:44:49 -0500

    heredoc

--- langs/Bash/for-loops.sh
+++ langs/Bash/for-loops.sh
@@ -9,6 +9,23 @@ while read -r line; do
     echo "<$line>"
 done < <(ls -1 ./direcotry)
 
+while read -r line; do
+    date -d @$line
+done << EOM
+1743255907
+1743255930
+1743256054
+1743256316
+1743271920
+1743272034
+1743272063
+1743272280
+1743272388
+1743272482
+1743272585
+1743272945
+EOM
+
 # bad
 for token in $(cat data.txt); do
     echo "<$token>"
