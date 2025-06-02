1271769d8c91918b7ddec454c6fa23cc824fdc38
treatmesubj <jrock4503@hotmail.com>
2025-06-02 09:28:19 -0500

    yq splitting

--- Linux/jq-yq/yq-split-yaml-into-files.sh
+++ Linux/jq-yq/yq-split-yaml-into-files.sh
@@ -1,1 +1,11 @@
 mkdir split && yq '.spec.templates[]' headcount-monthly-eid-template.yaml -s '"./split/" + .templateName'
+
+#########
+# almost
+yq '.spec.templates[] as $i | [$i | parent(3) | .spec.templates = $i]' headcount-monthly-eid-template.yaml | nvim -c "set ft=yaml"
+
+# works
+yq '[.spec.templates[] as $i | [$i | parent(3) | .spec.templates = $i][0]] | .[] | split_doc' headcount-monthly-eid-template.yaml | nvim -c "set ft=yaml"
+
+# everything works
+mkdir split && yq '[.spec.templates[] as $i | [$i | parent(3) | .spec.templates = [$i]][0]] | .[] | split_doc' headcount-monthly-eid-template.yaml -s '"./split/" + .spec.templates[0].templateName'
