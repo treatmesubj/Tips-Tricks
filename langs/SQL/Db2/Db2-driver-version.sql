764a2c5d651a75a6cebbb618b63aec2da9acf5cf
treatmesubj <jrock4503@hotmail.com>
2025-09-18 14:15:13 -0500

    db2 zos

--- langs/SQL/Db2/Db2-driver-version.sql
+++ langs/SQL/Db2/Db2-driver-version.sql
@@ -4,5 +4,10 @@
 -- java -cp ./db2jcc4.jar com.ibm.db2.jcc.DB2Jcc -version
 -- # IBM Data Server Driver for JDBC and SQLJ 4.25.13
 
+-- Db2 LUW
 SELECT service_level, fixpack_num
 FROM TABLE (sysproc.env_get_inst_info()) as INSTANCEINFO
+-- FROM SYSIBMADM.ENV_INST_INFO
+
+-- Db2 z/OS
+SELECT GETVARIABLE('SYSIBM.VERSION') FROM SYSIBM.SYSDUMMY1
