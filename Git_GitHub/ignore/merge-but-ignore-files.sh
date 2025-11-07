7af56174768e39964e121ab6d2e233b8d72ffbe6
treatmesubj <jrock4503@hotmail.com>
2025-11-07 12:37:19 -0600

    restore from another branch

--- Git_GitHub/ignore/merge-but-ignore-files.sh
+++ Git_GitHub/ignore/merge-but-ignore-files.sh
@@ -4,3 +4,6 @@ git reset HEAD file-to-ignore.txt  # un-stage file
 git checkout -- file-to-ignore.txt  # discard any changes made by the merge
 # or if new file to branch, just do: rm file-to-ignore.txt
 git commit -m "merged feat-branch"
+
+# copy file from another branch without staging
+git restore --source master-johnh run-uif-on-cluster.sh
