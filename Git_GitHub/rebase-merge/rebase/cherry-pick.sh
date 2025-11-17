8cddb13746ecea3b637a382f52905067841bd48b
treatmesubj <jrock4503@hotmail.com>
2025-11-17 09:32:23 -0600

    cherry-pick

--- Git_GitHub/rebase-merge/rebase/cherry-pick.sh
+++ Git_GitHub/rebase-merge/rebase/cherry-pick.sh
@@ -5,3 +5,8 @@ git cherry-pick <hash>
 # Auto-merging <file>
 # Auto-merging <file>
 # Auto-merging <file>
+
+# messed up git log somehow, reset & play a series commits on master
+git log-lil -10
+git reset --hard origin/master
+git cherry-pick <ref-a>^..<ref-b>  # from & including ref-a through ref-b
