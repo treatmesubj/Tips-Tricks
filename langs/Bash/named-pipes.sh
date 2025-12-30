22e2e566c75834a486a9f6248c633455fa5cff67
treatmesubj <jrock4503@hotmail.com>
2025-12-30 13:15:30 -0600

    file descriptor & fifo notes

--- langs/Bash/named-pipes.sh
+++ langs/Bash/named-pipes.sh
@@ -1,21 +1,44 @@
 #!/usr/bin/env bash
 
+# named-pipes are FIFO data-streams.
+# each byte can only be read exactly once.
+# so, if you have multiple readers of a named-pipe,
+# OS kernel will load-balance between readers;
+# only one reader will receive each output.
+# named-pipes do not broadcast each output to all readers.
+#
+# named-pipes safely used by multiple writers, single reader.
+
+# basic example
 # mkfifo ./pipey
-exec {fd}<>pipey
+# echo hello > ./pipey  # hangs until read
+# cat ./pipey
+
+# cat ./pipey # hangs until something to read
+# echo hello > ./pipey
+
+# open my named-pipe ./pipey for both reading and writing
+# as a random file-descriptor stored in variable 'fd'
+exec {fd}<>./pipey
+echo "file-descriptor 'fd' is: $fd"
 
 client() {
     local name=$1
 
     while true; do
         sleep 3
-        echo "[client $name] hello" >&$fd
+        echo "[client $name] hello" >&"$fd"
     done
 }
 
-client foo &
-client bar &
-client baz &
+client foo &  # background job
+client bar &  # background job
+client baz &  # background job
 
+echo "background jobs are: "
+jobs
+
+echo "now reading file-descriptor $fd"
 while read -r -u "$fd" line; do
     echo "[read line] $line"
 done
@@ -30,4 +53,3 @@ done
 #   20370 pts/3    S+     0:00      |   \_ sleep 3
 #   20260 pts/3    S+     0:00      \_ bash ./named-pipes.sh
 #   20371 pts/3    S+     0:00          \_ sleep 3
-
