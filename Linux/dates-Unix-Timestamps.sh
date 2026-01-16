7cd904980620a3db1a836b8ad48d1e91cbeb9061
treatmesubj <jrock4503@hotmail.com>
2026-01-16 08:29:56 -0600

    timestamp timezone convert

--- Linux/dates-Unix-Timestamps.sh
+++ Linux/dates-Unix-Timestamps.sh
@@ -1,3 +1,6 @@
 date +%s%3N  # Unix time - now milliseconds
 date -d "yesterday" +%s%3N  # Unix time - yesterday milliseconds
 date -d "tomorrow" +%s%3N  # Unix time - tomorrow milliseconds
+
+# convert UTC timestamp to local time
+date -d 'TZ="UTC" 2026-01-16T13:39:39'
