a9dccec170ae6a1ac9afcae7b2fe08820d53779e
treatmesubj <jrock4503@hotmail.com>
2026-04-11 20:03:12 -0500

    wayzata

---
+++ langs/Bash/scripts/mn-schools-rep-card.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# get all schools
+curl -s "https://rc.education.mn.gov/ibi_apps/WFServlet?IBIAPP_app=rptcard_reports&IBIF_ex=rptcard_getfilter_orglist.fex&reportCode=9" > all-schools.json
+
+# filter for high schools or Wayzata district
+# yq -r -p json '[ .schools | with_entries(select(.[].parentName | test("Wayzata"))).[] | {"id": key, "name": .name} ]' \
+yq -r -p json '[ .schools | with_entries(select(.[].name | test("High$|High School"))).[] | {"id": key, "name": .name} ]' \
+    -o tsv < all-schools.json > schools.tsv
+
+# exclude junior highs
+grep -v -i "junior" schools.tsv | sponge schools.tsv
+
+# chop headers
+sed -i '1d' schools.tsv
+
+# fetch & write out results
+IFS=$'\t'
+while read -r line; do
+    arr=( $(echo "$line") )
+    schoolid=${arr[0]}
+    schoolname=${arr[1]}
+    echo $schoolid $schoolname
+    # get school's data
+    curl -s "https://rc.education.mn.gov/ibi_apps/WFServlet?IBIAPP_app=rptcard_reports&IBIF_ex=rptcard_getdata_myschool&orgName=Statewide&orgId=$schoolid&groupType=school" \
+        | jq -r --arg schoolname $schoolname \
+            '[ .dataSets.Assessment.data.[].[] ].[] + {"school": $schoolname} | [ keys[] as $k | .[$k] ] | @csv' \
+            >> results.csv
+    sleep 1
+done < schools.tsv
+unset IFS
+
+# add headers
+sed -i '1i\subject_code,subject_name,year,proficient_count,proficient_percent,proficient_percent,school_name,tested_count' results.csv
