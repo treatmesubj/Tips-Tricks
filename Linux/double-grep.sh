8b9a1f8f8f2825364fd2dde86f9123b737d79aa9
treatmesubj <jrock4503@hotmail.com>
2026-05-13 09:18:53 -0500

    double rg

--- Linux/double-grep.sh
+++ Linux/double-grep.sh
@@ -3,3 +3,7 @@ for i in $(grep -irl stringy1); do printf "\n*********\n$i\n" && grep -i stringy
 for i in $(rg -il stringy1); do printf "\n*********\n$i\n" && rg -i stringy2 $i; done
 
 parallel rg -il findduplicate :::: <(rg -il addsurrogatekey) | wc -l
+
+
+parallel --tty rg -g {1} -io {2} :::: <(rg -il wf360-prod) :::: wf360-dep-fields.txt
+parallel --tty rg -g '{1}' -io "WF360_HR\.{2}\.*" :::: <(rg -il wf360-prod) :::: wf360-dep-tables.txt
