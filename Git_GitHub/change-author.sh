acef5f110f836d162f27c243f157c96e4d5bbe27
treatmesubj <jrock4503@hotmail.com>
2026-07-09 16:24:06 -0500

    clarity

--- Git_GitHub/change-author.sh
+++ Git_GitHub/change-author.sh
@@ -5,7 +5,6 @@ git config user.email "john@doe.org"
 # change last commit's author
 git commit --amend --author="John Doe <john@doe.org>"
 
-# reset the author for all commits after the given BASE_SHA:
-
+# reset author for a given commit's BASE_SHA and all following commits:
 git rebase -i BASE_SHA -x \
   "git commit --amend --author 'John Doe <johndoe@example.com>' -CHEAD"
