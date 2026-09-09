9e9090f33a50e23a9a488e179290611d88c79a1f
treatmesubj <jrock4503@hotmail.com>
2026-09-09 14:52:28 -0500

    pretty stdout

--- Data_Eng/parquets/diff-parquet-schemas.sh
+++ Data_Eng/parquets/diff-parquet-schemas.sh
@@ -8,32 +8,58 @@ duckdb -csv -noheader -c "
 
 [[ -s fileys.txt ]] || { echo "Error: No files found" >&2; exit 1; }
 
-# extract metadata
+echo $(wc -l < fileys.txt) files
+
+echo "
+#####################
+# fetching metadata #
+#####################
+"
+
+# fetch metadata
 mkdir -p metadata
 while read -r filey; do
-    echo fetching metadata for "$filey"
+    echo "$filey"
+    base="$(basename $filey)"
     duckdb -csv -c "SELECT row_group_num_columns, column_id, path_in_schema, type, compression FROM parquet_metadata('$filey');" < /dev/null \
-         > ./metadata/"$(basename "$filey")".csv
+         > ./metadata/${base%%.*}.csv
 done < fileys.txt
 
-baseline=$(find ./metadata/ -type f | head -1)
+echo "
+#####################
+# diff'ing metadata #
+#####################
+"
 
 # compare metadata
+baseline=$(find ./metadata/ -type f | head -1)
 while read -r fili; do
-    diff -us "$baseline" "$fili" | grep --color=always -E 'identical|$'
+    diff -us --color=always -L "BASELINE" "$baseline" "$fili" | grep --color=always -E 'identical|$'
 done < <(find ./metadata -type f)
 
-# extract schemas
+echo "
+###################
+# fetching shemas #
+###################
+"
+
+# fetch schemas
 mkdir -p schema
 while read -r filey; do
-    echo fetching schema for "$filey"
+    echo "$filey"
+    base="$(basename $filey)"
     duckdb -csv -c "SELECT * EXCLUDE file_name FROM parquet_schema('$filey')" < /dev/null \
-         > ./schema/"$(basename "$filey")".csv
+         > ./schema/${base%%.*}.csv
 done < fileys.txt
 
-baseline=$(find ./schema/ -type f | head -1)
+echo "
+####################
+# diff'ing schemas #
+####################
+"
 
 # compare schemas
+baseline=$(find ./schema/ -type f | head -1)
 while read -r fili; do
-    diff -us "$baseline" "$fili" | grep --color=always -E 'identical|$'
+    diff -us --color=always -L "BASELINE" "$baseline" "$fili" | grep --color=always -E 'identical|$'
 done < <(find ./schema -type f)
